bf315dcf3bd239062bfd82585292329d0a399233
adrianclinansmith <adrianclinansmith@hotmail.com>
2020-04-06 17:18:08 -0400

    Extended Babylonian Method works now

--- radicals/ExtendedBabylonianMethod.m
+++ radicals/ExtendedBabylonianMethod.m
@@ -2,16 +2,18 @@
 % Extended Babylonian Method: finding the nth root
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-n = 3;       % We will calculate the nth root. 
-a = 2;       % The number we want the nth root of.
+n = 17;      % We will calculate the nth root. 
+a = 2;       % The number we want the nth root of: a^(1/n).
 x = 1;       % An initial guess.
 last_x = -1; % If the last value & current value are close, we stop.
 
 while (abs(last_x - x) > 0.0001) 
   
-  last_x = x
-  x = 0.5*( x + a/x^(n-1) )
+  last_x = x;
+  x = (1/n)*( (n-1)*x + a/x^(n-1) );
   
 end
 
 display(x);
+
+
